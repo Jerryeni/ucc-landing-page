49ea1a4b4f59ccf2d27aafec5e1ce8519c64f380
Jeremiah Ibiang Eni <jerryeni3@gmail.com>
2024-12-10 17:59:39 +0100

    updates on the ui

--- components/alert-banner.tsx
+++ components/alert-banner.tsx
@@ -9,26 +9,24 @@ export function AlertBanner() {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50">
-      <div className="bg-secondary rounded-2xl shadow-lg overflow-hidden">
-        <div className="px-8 py-4 flex flex-col items-center justify-between space-x-8">
-          <span className="text-white text-lg font-medium">
+    <div className="fixed w-[80%] md:w-auto mx-auto bottom-6 left-1/2 transform -translate-x-1/2 z-50">
+      <div className="bg-[#F0B90B]  space-y-4 w-full rounded-2xl shadow-lg p-8 overflow-hidden">
+          <span className="text-black text-lg font-medium">
             Join Pre-sale – Earn X10 at Listing.
           </span>
-          <div className="flex w-full items-center space-x-4">
+          <div className="flex items-center space-x-4">
             <button
-              className="bg-white text-black px-6 py-2 rounded-lg font-semibold hover:bg-gray-100 transition-colors"
+              className="bg-white text-black w-full texr-xs px-6 py-2 rounded-lg font-semibold hover:bg-gray-100 transition-colors"
             >
               Buy $UCC Now
             </button>
             <button
               onClick={() => setIsVisible(false)}
-              className="text-white/50 hover:text-white transition-colors"
+              className="text-black hover:text-white transition-colors"
             >
               <X className="h-5 w-5" />
             </button>
           </div>
-        </div>
       </div>
     </div>
   );
--- components/ui/amount-input.tsx
+++ components/ui/amount-input.tsx
@@ -22,10 +22,10 @@ export function AmountInput({
         value={value}
         onChange={(e) => onChange(e.target.value)}
         readOnly={readOnly}
-        className="w-full bg-black/50 border border-[#F0B90B]/10 rounded-xl px-4 py-5 pr-24 text-lg"
+        className="w-full bg-secondary border border-[#F0B90B]/10 rounded-xl px-4 py-5 pr-24 text-lg"
         placeholder="0"
       />
-      <div className="absolute right-3 top-1/2 bg-secondary p-2 rounded-xl -translate-y-1/2 flex items-center gap-2">
+      <div className="absolute right-3 top-1/2 bg-input p-2 rounded-xl -translate-y-1/2 flex items-center gap-2">
         <img src={tokenIcon} alt={token} className="w-6 h-6" />
         <span>{token}</span>
       </div>
